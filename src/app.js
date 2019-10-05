d26b3f760847d9768f40a468f78ba54d161e8ff1
yessimov-a <a.yessimov98@gmail.com>
2019-10-05 21:23:23 +0600

    init: start repair-design-project

---
+++ src/app.js
@@ -0,0 +1,2 @@
+import './sass/style.scss';
+console.log('Hello, Aza');
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new CopyWebpackPlugin([
+            { from: 'src/assets', to: 'assets' }
+          ])
+    ]
+};
