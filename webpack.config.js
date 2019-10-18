cdf5c639d8375a19c7309a5807ea38846f6d9a11
yessimov-a <a.yessimov98@gmail.com>
2019-10-18 20:58:15 +0600

    fix: webpack fix and clear start project

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,62 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+  entry: [
+    './src/js/index.js',
+    './src/scss/style.scss'
+  ],
+  output: {
+    filename: './js/bundle.js'
+  },
+  devtool: "source-map",
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src/js'),
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: 'env'
+        }
+      }
     },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader']
-                })
-            }
+    {
+      test: /\.(sass|scss)$/,
+      include: path.resolve(__dirname, 'src/scss'),
+      use: ExtractTextPlugin.extract({
+        use: [{
+          loader: "css-loader",
+          options: {
+            sourceMap: true,
+            url: false
+          }
+        },
+        {
+          loader: "sass-loader",
+          options: {
+            sourceMap: true
+          }
+        }
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin('style.css'),
-        new CopyWebpackPlugin([
-            { from: 'src/assets', to: 'assets' }
-          ])
+      })
+    }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './css/style.bundle.css',
+      allChunks: true,
+    }),
+    new CopyWebpackPlugin([{
+      from: './src/assets/fonts',
+      to: './assets/fonts'
+    },
+    {
+      from: './src/assets/images',
+      to: './assets/img'
+    }
+    ]),
+  ]
 };
